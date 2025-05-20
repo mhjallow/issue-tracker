097e5bc29d72f522ad48ae43b226cbca3ed49837
Mohamed Jallow <43049419+mhjallow@users.noreply.github.com>
2025-05-20 18:50:12 -0400

    Scaffold frontend

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Issue Tracker App: Day 1</h1>
+    </div>
+  );
+}
+export default App;
