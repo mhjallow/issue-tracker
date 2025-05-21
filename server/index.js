7d04a238e1175633fe2ee1159207fb97be45286d
Mohamed Jallow <43049419+mhjallow@users.noreply.github.com>
2025-05-21 14:48:29 -0400

    Add server and refactor repository

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+// this is your first "route"
+app.get("/", (req, res) => {
+  res.send("Server is working!");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
