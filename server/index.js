31ae3f7a7568ff08f179219f41397ebd4b473679
Mohamed Jallow <43049419+mhjallow@users.noreply.github.com>
2025-05-23 00:22:16 -0400

    Add comments to App.tsx

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
-const express = require("express");
+const express = require("express"); //Loads the express tool
 const cors = require("cors"); //Loads the CORS tool. This is necessary for now becuase the front and back ends run on two different local ports.
 
-const app = express();
+const app = express(); //Creating an express application
 
 // Enable CORS
 app.use(cors());
 
+
+//When someone visits the address "/" send back a message
 app.get("/", (req, res) => {
   res.send("Your server is working!");
 });
 
-const PORT = 5000;
-app.listen(PORT, () => {
+
+const PORT = 5000; //Storing the port number
+
+
+app.listen(PORT, () => { //Start listening for requests on the port number, and run the code inside once the server is running
   console.log(`Server is running on port ${PORT}`);
 });
