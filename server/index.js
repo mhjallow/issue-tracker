d1b772ccf527ae71d0e2c1393d3a047ed8e75aed
Mohamed Jallow <43049419+mhjallow@users.noreply.github.com>
2025-05-23 00:08:11 -0400

    Add CORS

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const app = express();
-const PORT = 5000;
+const cors = require("cors"); //Loads the CORS tool. This is necessary for now becuase the front and back ends run on two different local ports.
 
-// this is your first "route"
+const app = express();
+
+// Enable CORS
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("Server is working!");
+  res.send("Your server is working!");
 });
 
-
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
