cddc9fc2f9974027550e6e3f8adc311296b117cb
Mohamed Jallow <43049419+mhjallow@users.noreply.github.com>
2025-05-23 00:04:51 -0400

    Display backend message on frontend with fetch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,24 @@
-function App() {
-  return (
+import { useEffect, useState } from "react";
+// - useEffect: Runs code when the component shows up (like "on page load")
+// - useState: Creates a "memory" which stores information temporarily
+
+function App() { //The top level piece of the application, the visible part of the app.
+
+  //Creates a memory box named message. setMessage is the tool used to change the content of the message later. We start with an empty string.
+  const [message, setMessage] = useState("");
+
+  useEffect(() => { //Runs the code inside once, when the page first loads
+    fetch("http://localhost:5000/") //sending a fetch request to the server
+      .then((response) => response.text()) //taking the response and converting it to plain text
+      .then((data) => setMessage(data))//Whatever comes back gets saved into the memory box using setMessage
+      .catch((error) => console.error("Error fetching data:", error)); //error handling so the app doesn't crash
+  }, []); //The '[]' means don't run this again unless something changes
+
+  return ( //returning the HTML structure to display things on the page
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">Issue Tracker App: Day 1</h1>
+      <h1 className="text-2xl font-semibold text-green-700">{message}</h1>
     </div>
   );
 }
+
 export default App;
